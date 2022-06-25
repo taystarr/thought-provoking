09f6cff3d538d159883dac03a44298a39155668c
Taylor Roberts <tstarrroberts@gmail.com>
2022-06-25 01:48:40 -0400

    add server and npm packages

---
+++ server.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017//thought-provoking', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
