5577fddb59c186a322fcfa263a854af2682f96d5
Taylor Roberts <tstarrroberts@gmail.com>
2022-06-26 20:21:34 -0400

    add user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -38,7 +38,63 @@ const userController = {
             .catch(err => res.status(400).json(err));
     },
     // update user
-
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, {new: true, runValidators: true })
+            .then(dbUserData => {
+                if(!dbUserData) {
+                    res.status(404).json({ message: 'no user found with this id' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err))
+    },
     // delete user
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+            .then(dbUserData => {
+                if(!dbUserData) {
+                    res.status(404).json({ message: 'no user found with this id' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
+    // add friend
+    addFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.id }, 
+            { $push: { friends: params.id.friendId}},
+            { new: true })
+                .populate({ path: 'friends', select: ('-__v')})
+                .select('-__v')
+                .then(dbUserData => {
+                    if(!dbUserData) {
+                        res.status(404).json({ message: 'no user found with this id' });
+                        return;
+                    }
+                    res.json(dbUserData);
+                })
+                .catch(err => res.json(err));
+    },
+    // delete friend
+    deleteFriend({ params }, res) {
+        User.findOneAndDelete(
+            { _id: params.id }, 
+            { $pull: { friends: params.friendId }}, 
+            { new: true })
+                .populate({ path: 'friends', select: '-__v' })
+                .select('-__v')
+                .then(dbUserData => {
+                    if(!dbUserData) {
+                        res.status(404).json({ message: 'no user found with this id' });
+                        return;
+                    }
+                    res.json(dbUserData);
+                })
+                .catch(err => res.status(400).json(err));
+    }
+};
 
-}
+module.exports = userController;
